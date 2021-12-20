33d75b86a0d64865023f11aa2b950459f7279475
Aleksandr Aniskevych <aleksandr@users-MacBook-Pro.local>
2021-12-20 13:11:52 +0200

    переменные

---
+++ app.js
@@ -0,0 +1,23 @@
+// alert(1);
+// Переменные  
+const para = document.querySelector("h1");
+para.addEventListener("click", updateName);
+
+function updateName() {
+    let name = prompt("Enter a new name");
+    para.textContent = name + " - суперчувак";
+}
+var name = "Aleks"
+const lastName = "Aniskevich"
+const age = 26 //namber
+age = "same string"
+const isProgrammer = true //boolean
+//camelCase
+const firstName = "Aleksandr"
+// const if = "mkef" //err
+const withNum5 = "5"
+// const 5withNum = "5" // err
+// console.log(age) 
+
+//Мутирование
+ console.log( )
