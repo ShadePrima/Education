f00cedb59bfaf1903d9e191118c32e27dab02f04
Aleksandr Aniskevych <aleksandr@users-MacBook-Pro.local>
2021-12-20 17:04:51 +0200

    операторы

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // alert(1);
-// Переменные  
+// 1 Переменные  
+
 const para = document.querySelector("h1");
 para.addEventListener("click", updateName);
 
@@ -7,17 +8,57 @@ function updateName() {
     let name = prompt("Enter a new name");
     para.textContent = name + " - суперчувак";
 }
-var name = "Aleks"
-const lastName = "Aniskevich"
-const age = 26 //namber
-age = "same string"
-const isProgrammer = true //boolean
+// var name = "Aleks"
+// const lastName = "Aniskevich"
+// let age = 26 //namber
+// age = 37
+// const isProgrammer = true //boolean
 //camelCase
-const firstName = "Aleksandr"
+// const firstName = "Aleksandr"
 // const if = "mkef" //err
-const withNum5 = "5"
+// const withNum5 = "5"
 // const 5withNum = "5" // err
 // console.log(age) 
 
-//Мутирование
- console.log( )
+// 2 Мутирование
+
+// console.log('Имя человека: ' + firstName + ',а возраст человека:' + age)
+// alert('Имя человека: ' + firstName + ',а возраст человека:' + age) 
+// console.log(age.toString()) 
+// prompt('Введите фамилию')
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + '' + lastName)
+
+// 3 Операторы
+// let currentYear = 2021
+// const birthYear = 1984
+
+// const age = currentYear + birthYear
+
+// const a = 10
+// const b = 5
+// let c = 32
+// // c = c + a
+// c += a
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(++currentYear)
+// console.log(--currentYear)
+// console.log(c)
+
+// Типы данных
+const isProgramer = true
+const name = 'Aleksandr'
+
+console.log()
+
+
+
+
+
+
+
+ 
