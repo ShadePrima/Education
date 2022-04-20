840cf75ec07ce2b4ecdb715a56cf4006c45476b6
ShadePrima <aleksandrag1984@gmail.com>
2022-04-20 19:49:30 +0300

    button

---
+++ rolli-shop-start-project/js/counter.js
@@ -0,0 +1,9 @@
+const buttonMinus = document.querySelector('[data-action="minus"]')
+const buttonPlus = document.querySelector('[data-action="plus"]')
+
+buttonMinus.addEventListener('click', function() {
+    
+})
+
+
+
