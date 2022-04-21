d92078ed2103dbc0c10261b14dbf6d59817676fa
ShadePrima <aleksandrag1984@gmail.com>
2022-04-21 22:02:28 +0300

    added products to bascet

---
+++ rolli-shop-start-project/js/card-01.js
@@ -0,0 +1,56 @@
+//Отслеживаем клик на странице 
+
+//Див внутри корзины в который мы добавляем товары 
+const cartWrapper = document.querySelector('.cart-wrapper')
+
+window.addEventListener('click', function(event) {
+    //Проверяем что клик был совершен по кнопке добавить в корзину
+    if (event.target.hasAttribute('data-cart')) {
+        console.log('Click on card')
+        //находим карточку с товаром внутри которой был совершен клик 
+        const card = event.target.closest('.card')
+
+        //Собираем данные с карточки товара и записываем их в единый обьект productInfo
+
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.product-img').getAttribute('src'),
+            title:  card.querySelector('.item-title').innerText,
+            itemsInBox: card.querySelector('[data-items-in-box]').innerText,
+            weight: card.querySelector('.price__weight').innerText,
+            price: card.querySelector('.price__currency').innerText,
+            counter: card.querySelector('[data-counter]').innerText
+        }
+        console.log(productInfo)
+
+        //Собранные дынные подставим в шаблон корзины 
+        
+        const cardItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+        <div class="cart-item__top">
+            <div class="cart-item__img">
+                <img src="${productInfo.imgSrc}" alt="${productInfo.title}">
+            </div>
+            <div class="cart-item__desc">
+                <div class="cart-item__title">${productInfo.title}</div>
+                <div class="cart-item__weight">${productInfo.itemsInBox} / ${productInfo.weight}.</div>
+
+                <!-- cart-item__details -->
+                <div class="cart-item__details">
+
+                    <div class="items items--small counter-wrapper">
+                        <div class="items__control" data-action="minus">-</div>
+                        <div class="items__current" data-counter="">${productInfo.counter}</div>
+                        <div class="items__control" data-action="plus">+</div>
+                    </div>
+
+                    <div class="price">
+                        <div class="price__currency">${productInfo.price} ₽</div>
+                    </div>
+
+                </div>`
+
+                // Отобразим данные в корзине 
+                cartWrapper.insertAdjacentHTML('beforeend', cardItemHTML)
+
+    }
+})   
--- rolli-shop-start-project/js/counter-02.js
+++ rolli-shop-start-project/js/counter-02.js
@@ -1,13 +1,23 @@
 //Добавление прослушки на всем окне  
 window.addEventListener('click', function(event) {
-    const counterWrapper = event.target.closest('.counter-wrapper')
-    const counter = counterWrapper.querySelector('[data-counter]')
+    //обьявляем переменную для счетчика 
+    let counter
+    
+
+    //Проверяем ли приходится ли клик строго по кнопкам плюс или минус 
+    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+        console.log(true)
+        const counterWrapper = event.target.closest('.counter-wrapper')
+        counter = counterWrapper.querySelector('[data-counter]')
+    }
+
+
 
     //проверяем является ли элмент по которому кликнули кнопкой плюс 
     if (event.target.dataset.action === 'plus') {
         counter.innerText = ++counter.innerText
     }
-    
+
     //проверяем является ли элемент по которому кликнули кнопкой минус 
     if (event.target.dataset.action === 'minus') {
         if (parseInt(counter.innerText) > 1) {
